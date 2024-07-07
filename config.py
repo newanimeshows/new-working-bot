536802cd55083860c84883b7625da58a7bbdba96
AnmolPandita-sudo <82043884+AnmolPandita-sudo@users.noreply.github.com>
2024-07-07 22:23:36 +0530

    updated

--- config.py
+++ config.py
@@ -11,6 +11,8 @@ from logging.handlers import RotatingFileHandler
 from dotenv import load_dotenv
 from pymongo import MongoClient
 import pymongo
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
 
 load_dotenv()
@@ -95,6 +97,14 @@ try:
     print("Connected to MongoDB!")
 except Exception as e:
     print(f"Error connecting to MongoDB: {e}")
+
+client = MongoClient(DB_URL, server_api=ServerApi('1'))
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 
 # Jishu Developer
