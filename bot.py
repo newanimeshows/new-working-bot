1554e0068e3dc34f907427ac14dc19f27f6cc01a
AnmolPandita-sudo <82043884+AnmolPandita-sudo@users.noreply.github.com>
2024-07-08 14:42:49 +0530

    updated

--- bot.py
+++ bot.py
@@ -80,7 +80,6 @@ class Bot(Client):
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
-        print("Working")
 
     async def stop(self, *args):
         await super().stop()
