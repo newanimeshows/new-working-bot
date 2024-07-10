27dd2c3f86a46a8306223bc0a82845de30829934
AnmolPandita-sudo <82043884+AnmolPandita-sudo@users.noreply.github.com>
2024-07-10 13:00:37 +0530

    updated

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -5,6 +5,7 @@ from config import ADMINS
 from helper_func import encode, get_message_id
 
 
+@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
         try:
