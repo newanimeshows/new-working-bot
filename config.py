63f5a2abbf1d95986904e1917a3923368f6db2b2
AnmolPandita-sudo <82043884+AnmolPandita-sudo@users.noreply.github.com>
2024-07-10 22:18:18 +0530

    updated

--- config.py
+++ config.py
@@ -27,7 +27,8 @@ DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002174448712"))
+CHANNEL_IDS = [int(id) for id in os.environ.get(
+    "CHANNEL_IDS", "-1002174448712,-1002182947491").split(",")]
 FORCE_SUB_CHANNEL1 = int(os.environ.get(
     "FORCE_SUB_CHANNEL1", "0"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get(
--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -3,50 +3,68 @@ from pyrogram import filters, Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 from bot import Bot
-from config import ADMINS, CHANNEL_ID, DISABLE_CHANNEL_BUTTON
+from config import ADMINS, CHANNEL_IDS, DISABLE_CHANNEL_BUTTON
 from helper_func import encode
 
 
 @Bot.on_message(filters.private & filters.user(ADMINS) & ~filters.command(['start', 'users', 'broadcast', 'batch', 'genlink', 'stats']))
 async def channel_post(client: Client, message: Message):
     reply_text = await message.reply_text("Please Wait...!", quote=True)
-    try:
-        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
-    except FloodWait as e:
-        await asyncio.sleep(e.x)
-        post_message = await message.copy(chat_id=client.db_channel.id, disable_notification=True)
-    except Exception as e:
-        print(e)
-        await reply_text.edit_text("Something went Wrong..!")
-        return
-    converted_id = post_message.id * abs(client.db_channel.id)
-    string = f"get-{converted_id}"
-    base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
 
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(
-        "🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+    post_messages = []
+    for channel_id in CHANNEL_IDS:
+        try:
+            post_message = await message.copy(chat_id=channel_id, disable_notification=True)
+            post_messages.append((channel_id, post_message))
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            post_message = await message.copy(chat_id=channel_id, disable_notification=True)
+            post_messages.append((channel_id, post_message))
+        except Exception as e:
+            print(e)
+            await reply_text.edit_text("Something went Wrong..!")
+            return
 
-    await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview=True)
+    links = []
+    for channel_id, post_message in post_messages:
+        converted_id = post_message.id * abs(channel_id)
+        string = f"get-{converted_id}"
+        base64_string = await encode(string)
+        link = f"https://t.me/{client.username}?start={base64_string}"
+        links.append(link)
 
-    if not DISABLE_CHANNEL_BUTTON:
-        await post_message.edit_reply_markup(reply_markup)
+        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(
+            "🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+
+        if not DISABLE_CHANNEL_BUTTON:
+            try:
+                await post_message.edit_reply_markup(reply_markup)
+            except Exception as e:
+                print(e)
+                pass
+
+    if links:
+        links_text = "\n\n".join(links)
+        await reply_text.edit(f"<b>Here are your links</b>\n\n{links_text}", reply_markup=reply_markup, disable_web_page_preview=True)
 
 
-@Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_ID))
+@Bot.on_message(filters.channel & filters.incoming & filters.chat(CHANNEL_IDS))
 async def new_post(client: Client, message: Message):
 
     if DISABLE_CHANNEL_BUTTON:
         return
 
-    converted_id = message.id * abs(client.db_channel.id)
-    string = f"get-{converted_id}"
-    base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(
-        "🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    try:
-        await message.edit_reply_markup(reply_markup)
-    except Exception as e:
-        print(e)
-        pass
+    for channel_id in CHANNEL_IDS:
+        if message.chat.id == channel_id:
+            converted_id = message.id * abs(channel_id)
+            string = f"get-{converted_id}"
+            base64_string = await encode(string)
+            link = f"https://t.me/{client.username}?start={base64_string}"
+            reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(
+                "🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
+            try:
+                await message.edit_reply_markup(reply_markup)
+            except Exception as e:
+                print(e)
+                pass
+            break
