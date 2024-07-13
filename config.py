6ac03226dfdd9eb815e9c8a478f184d7dcf1676e
joevaas <133934323+joevaas@users.noreply.github.com>
2024-07-13 21:25:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ DB_URL = os.environ.get("DB_URL", "")
 DB_NAME = os.environ.get("DB_NAME", "")
 
 
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002168169351"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002165223245"))
 FORCE_SUB_CHANNEL1 = int(os.environ.get(
     "FORCE_SUB_CHANNEL1", "0"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get(
