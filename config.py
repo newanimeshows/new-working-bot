a8e9551a9b98a11736dcc8de0941c5bfab310ec1
newanimeshows <artificialartist00@gmail.com>
2024-08-02 12:07:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "3"))
 
 # start message
 START_MSG = os.environ.get(
-    "START_MESSAGE", "<b>🌸 ᴀʀɪɢᴀᴛᴏᴜ, {first}!\n\n 🌸 ᴇᴍʙʀᴀᴄᴇ ᴛʜᴇ ᴀɴɪᴍᴇ ᴍᴀɢɪᴄ! ʜᴇʀᴇ, ᴘɪxᴇʟꜱ ᴅᴀɴᴄᴇ, ꜱᴛᴏʀɪᴇꜱ ᴜɴꜰᴏʟᴅ, ᴀɴᴅ ᴇᴍᴏᴛɪᴏɴꜱ ɪɢɴɪᴛᴇ. ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ꜱᴀɴᴄᴛᴜᴀʀʏ—ᴀ ᴘʟᴀᴄᴇ ᴡʜᴇʀᴇ ᴀɴɪᴍᴇ ᴅʀᴇᴀᴍꜱ ᴛᴀᴋᴇ ꜰʟɪɢʜᴛ. ᴇɴᴊᴏʏ ᴛʜᴇ ᴊᴏᴜʀɴᴇʏ! 🌟</b>")
+    "START_MESSAGE", "<b>🌸 𝙒𝙚𝙡𝙘𝙤𝙢𝙚, {first}!\n\n🌸 𝘿𝙞𝙫𝙚 𝙞𝙣𝙩𝙤 𝙩𝙝𝙚 𝙢𝙖𝙜𝙞𝙘 𝙤𝙛 𝙖𝙣𝙞𝙢𝙚 𝙬𝙝𝙚𝙧𝙚 𝙚𝙫𝙚𝙧𝙮 𝙨𝙩𝙤𝙧𝙮 𝙞𝙜𝙣𝙞𝙩𝙚𝙨 𝙮𝙤𝙪𝙧 𝙞𝙢𝙖𝙜𝙞𝙣𝙖𝙩𝙞𝙤𝙣. 𝙀𝙣𝙟𝙤𝙮 𝙮𝙤𝙪𝙧 𝙟𝙤𝙪𝙧𝙣𝙚𝙮 𝙬𝙞𝙩𝙝 𝙪𝙨! 🌟")
 
 try:
     ADMINS = [1196934318]
@@ -54,7 +54,7 @@ except ValueError:
 
 
 FORCE_MSG = os.environ.get(
-    "FORCE_SUB_MESSAGE", "<b><center> 🌸 𝙆𝙤𝙣𝙣𝙞𝙘𝙝𝙞𝙬𝙖, 𝙛𝙚𝙡𝙡𝙤𝙬 𝙤𝙩𝙖𝙠𝙪! 🌸 </center>\n\n 𝙄𝙛 𝙮𝙤𝙪’𝙧𝙚 𝙖 𝙩𝙧𝙪𝙚 𝙨𝙚𝙣𝙥𝙖𝙞, 𝙟𝙤𝙞𝙣 𝙢𝙮 𝙘𝙝𝙖𝙣𝙣𝙚𝙡𝙨 𝙖𝙣𝙙 𝙡𝙚𝙩’𝙨 𝙬𝙚𝙖𝙫𝙚 𝙤𝙪𝙧 𝙖𝙣𝙞𝙢𝙚 𝙢𝙖𝙜𝙞𝙘 𝙩𝙤𝙜𝙚𝙩𝙝𝙚𝙧! 🎉 𝙇𝙚𝙩’𝙨 𝙨𝙥𝙧𝙞𝙣𝙠𝙡𝙚 𝙖 𝙡𝙞𝙩𝙩𝙡𝙚 “𝙠𝙖𝙬𝙖𝙞𝙞” 𝙞𝙣𝙩𝙤 𝙤𝙪𝙧 𝙘𝙤𝙣𝙫𝙚𝙧𝙨𝙖𝙩𝙞𝙤𝙣𝙨, 𝙗𝙚𝙘𝙖𝙪𝙨𝙚 𝙡𝙞𝙛𝙚 𝙞𝙨 𝙗𝙚𝙩𝙩𝙚𝙧 𝙬𝙞𝙩𝙝 𝙖 𝙩𝙤𝙪𝙘𝙝 𝙤𝙛 𝙅𝙖𝙥𝙖𝙣𝙚𝙨𝙚 𝙛𝙡𝙖𝙞𝙧. 🌟🇯🇵</b>")
+    "FORCE_SUB_MESSAGE", "<b><center> 🌸 Hello, 𝙛𝙚𝙡𝙡𝙤𝙬 𝙤𝙩𝙖𝙠𝙪! 🌸 </center>\n\n 𝙃𝙚𝙧𝙚 𝙔𝙤𝙪 𝙃𝙖𝙫𝙚 𝙏𝙤 𝙅𝙤𝙞𝙣 𝙊𝙪𝙧 𝟐 𝘾𝙝𝙖𝙣𝙣𝙚𝙡𝙨, 𝙏𝙝𝙚𝙣 𝙔𝙤𝙪 𝘾𝙖𝙣 𝙐𝙨𝙚 𝙏𝙝𝙞𝙨 𝘽𝙤𝙩❗ 🎉 𝙇𝙚𝙩’𝙨 𝙨𝙥𝙧𝙞𝙣𝙠𝙡𝙚 𝙖 𝙡𝙞𝙩𝙩𝙡𝙚 “𝑱𝑨𝑷𝑨𝑵𝑬𝑺𝑬 𝐓𝐎𝐔𝐂𝐇” 𝙞𝙣𝙩𝙤 𝙤𝙪𝙧 𝙘𝙤𝙣𝙫𝙚𝙧𝙨𝙖𝙩𝙞𝙤𝙣𝙨, 𝙗𝙚𝙘𝙖𝙪𝙨𝙚 𝙡𝙞𝙛𝙚 𝙞𝙨 𝙗𝙚𝙩𝙩𝙚𝙧 𝙬𝙞𝙩𝒉 𝒊𝒕🌟</b>")
 
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
@@ -65,7 +65,7 @@ DISABLE_CHANNEL_BUTTON = os.environ.get(
     "DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌ʙᴀᴋᴀᴀ! ᴅᴏɴ'ᴛ ꜱᴇɴᴅ ᴍᴇ ᴍᴇꜱꜱᴀɢᴇ"
+USER_REPLY_TEXT = "❌ 𝙄 𝘼𝙈 𝘼 𝘽𝙊𝙏 𝙋𝙇𝙀𝘼𝙎𝙀 𝘿𝙊𝙉𝙏 𝙎𝙀𝙉𝘿 𝙈𝙀 𝙈𝙀𝙎𝙎𝘼𝙂𝙀𝙎!!"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1768198143)
