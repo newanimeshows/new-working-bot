36342f28e57a49e7a35bc1cec2d2b2f1af27f5aa
newanimeshows <artificialartist00@gmail.com>
2024-08-18 17:55:02 +0530

    Update and rename ownermessage01.py to ownermessage.py

--- plugins/ownermessage.py
+++ plugins/ownermessage.py
@@ -1,6 +1,6 @@
 import os
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import Message
 from pyrogram.enums import ParseMode
 from bot import Bot
 from config import OWNER_ID  # Ensure OWNER_ID is defined in your config.py
@@ -13,9 +13,8 @@ async def help_command(client: Client, message: Message):
 
 - <b>/start</b> - Start the bot.
 - <b>/help</b> - Show this help message.
-- <b>/owner</b> - Get info about the bot owner.
+- <b>/owner</b> - Get information about the bot owner.
 - <b>/bots</b> - List other bots created by the owner.
-- <b>/owner_msg</b> - Get in touch with the bot owner.
     """
     await message.reply_text(
         help_text,
@@ -28,23 +27,12 @@ async def owner_msg_command(client: Client, message: Message):
     user_mention = message.from_user.mention
 
     await message.reply_text(
-        f"👋 Hey {user_mention},\n\nClick the button below and send your message to the owner.",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("✨ Continue ✨", callback_data="continue")]]
-        ),
-        parse_mode=ParseMode.HTML
-    )
-
-# Callback query handler for the "Continue" button
-@Bot.on_callback_query(filters.regex("continue"))
-async def continue_callback(client: Client, callback_query: CallbackQuery):
-    await callback_query.message.edit_text(
-        "Please send me the message you'd like to send to the owner.",
+        f"👋 Hey {user_mention},\n\nPlease send your message now, and it will be forwarded to the owner.",
         parse_mode=ParseMode.HTML
     )
 
     # Capture the next message from the user
-    @Bot.on_message(filters.private & filters.text & ~filters.command)
+    @Bot.on_message(filters.private & filters.text & ~filters.command, group=1)
     async def capture_message(client: Client, message: Message):
         # Forward the user's message to the owner
         await client.send_message(
@@ -55,7 +43,7 @@ async def continue_callback(client: Client, callback_query: CallbackQuery):
         await message.reply("✅ Your message has been sent to the owner. Thank you!", parse_mode=ParseMode.HTML)
 
         # Remove the capture_message handler after it has been used
-        Bot.remove_handler(capture_message, group=0)
+        Bot.remove_handler(capture_message, group=1)
 
 # /owner command
 @Bot.on_message(filters.command('owner') & filters.private)
@@ -63,8 +51,8 @@ async def owner_command(client: Client, message: Message):
     owner_info = """
 <b>✨ Bot Owner:</b> ✨
 
-👤 <b>Name:</b> Ayan
-📧 <b>Contact:</b> <a href='tg://user?id=1196934318'>@Ayan</a>
+👤 <b>Name:</b> [Owner Name]
+📧 <b>Contact:</b> [Owner's Contact Info]
 
 For more bots, type <b>/bots</b>.
     """
